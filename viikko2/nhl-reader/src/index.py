d0bf51237e2759dce78400dabfc256d6a16e8473
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2024-11-11 21:18:15 +0200

    Do exercise 13

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,14 +1,37 @@
 from player_reader import PlayerReader
 from player_stats import PlayerStats
+from rich import print
+from rich.prompt import Prompt
+from rich.table import Table
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    print("NHL statistics by nationality")
+    while True:
+        season = Prompt.ask("Select season [bright_magenta][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/][/bright_magenta]")
+        if season in ["2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25"]:
+            break
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
 
-    for player in players:
-        print(player)
+    while True:
+        nationality = Prompt.ask("Select nationality [bright_magenta][AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR/][/bright_magenta]")
+        if nationality == "exit":
+            break
+        if nationality in ["AUT", "CZE", "AUS", "SWE", "GER", "DEN", "SUI", "SVK", "NOR", "RUS", "CAN", "LAT", "BLR", "SLO", "USA", "FIN", "GBR"]:
+            players = stats.top_scorers_by_nationality(nationality)
+            table = Table(title=f"Top scorers of {nationality} season {season}")
+            table.add_column("name", style="bright_cyan")
+            table.add_column("team", style="purple3")
+            table.add_column("goals", style="green3")
+            table.add_column("assists", style="green3")
+            table.add_column("points", style="green3")
+
+            for player in players:
+                table.add_row(player.name, player.team, str(player.goals), str(player.assists), str((player.goals + player.assists)))
+
+            print(table)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -6,7 +6,6 @@ class Player:
         self.goals = int(dict['goals'])
         self.team = dict['team']
         self.games = int(dict['games'])
-        self.id = int(dict['id'])
 
     def __str__(self):
         return f"{self.name:22}{self.team:5}{self.goals:3} + {self.assists:2} = {self.goals + self.assists:3}"
